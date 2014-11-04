0db0f950d97fd359d97f38c21ab8cd0fb053360d
Stephan Hoermann <srhoermann@gmail.com>
2014-11-04 13:17:21 +1100

    execution: Adds customer pipeline using execution monad.

---
+++ maestro-example/src/main/scala/au/com/cba/omnia/maestro/example/CustomerExecution.scala
@@ -0,0 +1,60 @@
+//   Copyright 2014 Commonwealth Bank of Australia
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package au.com.cba.omnia.maestro.example
+
+import org.apache.hadoop.hive.conf.HiveConf.ConfVars._
+
+import au.com.cba.omnia.maestro.api.exec.MaestroExecution
+import au.com.cba.omnia.maestro.example.thrift.{Account, Customer}
+
+object CustomerExecution extends MaestroExecution[Customer] {
+  def execute(hdfsRoot: String, localRoot: String, archiveRoot: String): Execution[(LoadInfo, Long)] = {
+    val source      = "customer"
+    val domain      = "customer"
+    val tablename   = "customer"
+    val errors      = s"${hdfsRoot}/errors/${domain}"
+    val validators  = Validator.all[Customer]()
+    val filter      = RowFilter.keep
+    val cleaners    = Clean.all(
+      Clean.trim,
+      Clean.removeNonPrintables
+    )
+    val dateTable =
+      HiveTable(domain, "by_date", Partition.byDate(Fields.EffectiveDate) )
+    val catTable  =
+      HiveTable(domain, "by_cat", Partition.byField(Fields.Cat))
+
+    for {
+      uploadInfo <- upload(source, domain, tablename, "{table}_{yyyyMMdd}.txt", localRoot, archiveRoot, hdfsRoot)
+
+      if uploadInfo.continue
+      (pipe, loadInfo) <- load[Customer](
+        "|", uploadInfo.files, errors,
+        timeFromPath(".*/([0-9]{4})/([0-9]{2})/([0-9]{2}).*".r),
+        cleaners, validators, filter, "null"
+      )
+
+      if loadInfo.continue
+      count1 <- viewHive(dateTable)(pipe)
+      _      <- viewHive(catTable)(pipe)
+      _      <- hiveQuery(
+        "test", catTable,
+        Map(HIVEMERGEMAPFILES -> "true"),
+        s"INSERT OVERWRITE TABLE ${catTable.name} PARTITION (partition_cat) SELECT id, name, acct, cat, sub_cat, -10, effective_date, cat AS partition_cat FROM ${dateTable.name}",
+        s"SELECT COUNT(*) FROM ${catTable.name}"
+      )
+    } yield (loadInfo, count1)
+  }
+}
---
+++ maestro-example/src/test/scala/au/com/cba/omnia/maestro/example/CustomerExecutionSpec.scala
@@ -0,0 +1,56 @@
+//   Copyright 2014 Commonwealth Bank of Australia
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package au.com.cba.omnia.maestro.example
+
+import au.com.cba.omnia.thermometer.core.{ThermometerSpec, Thermometer}, Thermometer._
+import au.com.cba.omnia.thermometer.hive.HiveSupport
+import au.com.cba.omnia.thermometer.fact.PathFactoids._
+
+import au.com.cba.omnia.ebenezer.test.ParquetThermometerRecordReader
+
+import au.com.cba.omnia.maestro.api._
+import au.com.cba.omnia.maestro.test.Records
+import au.com.cba.omnia.maestro.core.task.LoadSuccess
+
+import au.com.cba.omnia.maestro.example.thrift.Customer
+
+object CustomerExecutionSpec extends ThermometerSpec with Records with HiveSupport { def is = s2"""
+
+Customer Execution
+==================
+
+  end to end pipeline        $pipeline
+
+"""
+
+  lazy val decoder = Macros.mkDecode[Customer]
+
+  def pipeline = {
+    val actualReader   = ParquetThermometerRecordReader[Customer]
+    val expectedReader = delimitedThermometerRecordReader[Customer]('|', "null", decoder)
+
+    withEnvironment(path(getClass.getResource("/customer").toString)) {
+      executesSuccessfully(CustomerExecution.execute(s"$dir/user", s"$dir/user", s"$dir/user/archive")) must_== ((
+        LoadSuccess(8, 8, 0),
+        8
+      ))
+
+      facts(
+        hiveWarehouse </> "customer.db" </> "by_date" ==> recordsByDirectory(actualReader, expectedReader, "expected" </> "customer" </> "by-date"),
+        hiveWarehouse </> "customer.db" </> "by_cat"  ==> recordsByDirectory(actualReader, expectedReader, "expected" </> "customer" </> "by-cat")
+      )
+    }
+  }
+}

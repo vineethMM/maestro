f36495476df8e66968d5439222e8b4619d6fa17c
Sam Roberts <sam.roberts.1983@gmail.com>
2015-04-09 14:15:20 +1000

    fix Errors scaladoc

--- maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/scalding/Errors.scala
+++ maestro-core/src/main/scala/au/com/cba/omnia/maestro/core/scalding/Errors.scala
@@ -34,19 +34,18 @@ object Errors extends FieldConversions {
    * fix your program so it only has one trap).
    *
    * The process is basically that we give up on types and hack away:
-   *   - Convert back to a regular pipe.
-   *   - Add a trap on the regular pipe, for routing all failures to an error source.
-   *   - Convert back to a typed pipe.
    *   - Go into untyped land by stripping off the "left" data constructor (this
-   *     means we end up with a TypedPipe[Any] really that will either have raw
-   *     failures, or success in a "right" data constructor. We do this so when
-   *     errors are written out they are raw, and don't have the \/-() constructor.
+   *     means we end up with a TypedPipe[Any]) that will either have raw failure
+   *     strings, or success in a "right" data constructor. We do this so errors
+   *     are written out raw, without the \/-() constructor.
    *   - Make the conversion strict via a call to fork.
+   *   - Add a trap on the regular pipe, for routing all failures to an error source.
    *   - Force errors into error pipe, by:
    *     - Doing a runtime match on rights, and returning the raw value.
    *     - For all other cases (i.e. our errors), throw an exception to trigger
-   *       the trap. This should neatly write out the error because of our
-   *       previous hack to strip off the left constructor.
+   *       the trap. The trap writes out the value in the pipe, not the exception,
+   *       but should neatly write out the error because of our previous hack to
+   *       strip off the left constructor.
    */
   def handle[A](p: TypedPipe[String \/ A], errors: Source with TypedSink[Any]): TypedPipe[A] =
     p

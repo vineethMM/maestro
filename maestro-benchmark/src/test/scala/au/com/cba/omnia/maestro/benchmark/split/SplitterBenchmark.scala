963a956d8e3ae498c64d9e97d9dfe265375a2389
Sam Roberts <sam.roberts.1983@gmail.com>
2015-04-17 08:54:39 +1000

    remove unnecesary import from SplitterBenchmark

--- maestro-benchmark/src/test/scala/au/com/cba/omnia/maestro/benchmark/split/SplitterBenchmark.scala
+++ maestro-benchmark/src/test/scala/au/com/cba/omnia/maestro/benchmark/split/SplitterBenchmark.scala
@@ -16,8 +16,6 @@ package au.com.cba.omnia.maestro.benchmark.split
 
 import java.io.Serializable
 
-import scala.util.Random
-
 import org.scalameter.api.{PerformanceTest, Gen}
 
 import au.com.cba.omnia.maestro.core.split.Splitter

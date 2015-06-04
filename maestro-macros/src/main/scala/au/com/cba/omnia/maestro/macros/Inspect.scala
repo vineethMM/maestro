3957eb82d387b53804244f565aa8abb639859b08
Stephan Hoermann <srhoermann@gmail.com>
2015-06-04 16:08:21 +1000

    Adds generic macro constructor function for Thrift structs.
    Adds `construct` to `Inspect` which can create code to create humbug or scrooge
    thrift struct given code to create values for the individual fields.

--- maestro-macros/src/main/scala/au/com/cba/omnia/maestro/macros/Inspect.scala
+++ maestro-macros/src/main/scala/au/com/cba/omnia/maestro/macros/Inspect.scala
@@ -16,6 +16,8 @@ package au.com.cba.omnia.maestro.macros
 
 import scala.reflect.macros.whitebox.Context
 
+import org.apache.commons.lang.WordUtils
+
 import com.twitter.scrooge.ThriftStruct
 
 import au.com.cba.omnia.humbug.HumbugThriftStruct
@@ -38,18 +40,6 @@ object Inspect {
   /** Gets all the fields of a Thrift struct sorted in order of definition.*/
   def fields[A <: ThriftStruct: c.WeakTypeTag](c: Context): List[(c.universe.MethodSymbol, String)] =
     fieldsUnsafe(c)(c.universe.weakTypeOf[A])
-
-  /** Does a type derive from ThriftStruct? */
-  def isThriftType[A: c.WeakTypeTag](c:Context): Boolean =
-    c.universe.weakTypeOf[A] <:< c.universe.weakTypeOf[ThriftStruct]
-
-  /** Abort unless A derives from ThriftStruct. */
-  def ensureThriftType[A: c.WeakTypeTag](c: Context): Unit =
-    // Since type bounds on macro implementations don't currently prevent the macro from expanding
-    // we need this workaround to avoid materializing encoders for primitives. 
-    if (!isThriftType[A](c))
-      c.abort(c.enclosingPosition, s"${c.universe.weakTypeOf[A].toString} does not derive ThriftStruct.")
-
   /** Same as fields but for any type where the type is assumed to be ThriftStruct.*/
   def fieldsUnsafe(c: Context)(typ: c.universe.Type): List[(c.universe.MethodSymbol, String)] = {
     import c.universe._
@@ -74,4 +64,108 @@ object Inspect {
   /** Same as methods but for any type where the type is assumed to be ThriftStruct.*/
   def methodsUnsafe(c: Context)(typ: c.universe.Type): List[c.universe.MethodSymbol] =
     indexedUnsafe(c)(typ).map({ case (method, _) => method })
+
+  /** Does a type derive from ThriftStruct? */
+  def isThriftType[A : c.WeakTypeTag](c: Context): Boolean =
+    c.universe.weakTypeOf[A] <:< c.universe.weakTypeOf[ThriftStruct]
+
+  /** Does a type derive from ThriftStruct? */
+  def isThriftTypeUnsafe(c: Context)(typ: c.universe.Type): Boolean =
+    typ <:< c.universe.weakTypeOf[ThriftStruct]
+
+  /** Abort unless `A`` derives from ThriftStruct. */
+  def ensureThriftType[A : c.WeakTypeTag](c: Context): Unit =
+    // Since type bounds on macro implementations don't currently prevent the macro from expanding
+    // we need this workaround to avoid materializing encoders for primitives.
+    if (!isThriftType[A](c))
+      c.abort(c.enclosingPosition, s"${c.universe.weakTypeOf[A].toString} does not derive ThriftStruct.")
+
+  /** Abort unless `A` derives from ThriftStruct. */
+  def ensureThriftTypeUnsafe(c: Context)(typ: c.universe.Type): Unit =
+    // Since type bounds on macro implementations don't currently prevent the macro from expanding
+    // we need this workaround to avoid materializing encoders for primitives. 
+    if (!isThriftTypeUnsafe(c)(typ))
+      c.abort(c.enclosingPosition, s"${typ.toString} does not derive ThriftStruct.")
+
+  /** Does a tupe derive from `HumbugThriftStruct`? */
+  def isHumbug[A : c.WeakTypeTag](c: Context): Boolean =
+    c.universe.weakTypeOf[A] <:< c.universe.weakTypeOf[HumbugThriftStruct]
+
+  /** Does a tupe derive from `HumbugThriftStruct`? */
+  def isHumbugUnsafe(c: Context)(typ: c.universe.Type): Boolean =
+    typ <:< c.universe.weakTypeOf[HumbugThriftStruct]
+
+  /**
+    * Creates the code to construct a thrift struct of the specified type given code to generate
+    * values for each field where the code in the provided fields corresponds to the order of fields
+    * in the thrift struct.
+    */
+  def constructIndexed[A <: ThriftStruct : c.WeakTypeTag](c: Context)(fields: List[c.universe.Tree]): c.universe.Tree =
+    constructIndexedUnsafe(c)(c.universe.weakTypeOf[A], fields)
+
+  /**
+    * Creates the code to construct a thrift struct of the specified type given code to generate
+    * values for each field where the code in the provided fields corresponds to the order of fields
+    * in the thrift struct.
+    */
+  def constructIndexedUnsafe(c: Context)(typ: c.universe.Type, fields: List[c.universe.Tree]): c.universe.Tree =
+    if (isHumbugUnsafe(c)(typ)) constructHumbugUnsafe(c)(typ, fields)
+    else                        constructScroogeUnsafe(c)(typ,fields)
+
+  /**
+    * Creates the code to construct a thrift struct of the specified type given code to generate
+    * values for each named field.
+    */
+  def constructNamed[A <: ThriftStruct : c.WeakTypeTag](c: Context)(fields: List[(String, c.universe.Tree)]): c.universe.Tree =
+    constructNamedUnsafe(c)(c.universe.weakTypeOf[A], fields)
+
+  /**
+    * Creates the code to construct a thrift struct of the specified type given code to generate
+    * values for each named field.
+    */
+  def constructNamedUnsafe(c: Context)(typ: c.universe.Type, fields: List[(String, c.universe.Tree)]): c.universe.Tree = {
+    val order  = Inspect.fieldsUnsafe(c)(typ).map(x => WordUtils.uncapitalize(x._2)).zipWithIndex.toMap
+    val sorted = fields.map { case (n, f) => (order(n), f)}.sortBy(_._1).map(_._2)
+
+    constructIndexedUnsafe(c)(typ, sorted)
+  }
+
+  /**
+    * Creates the code to construct a Humbug thrift struct of the specified type given code to
+    * generate values for each field.
+    */
+  def constructHumbugUnsafe(c: Context)(typ: c.universe.Type, fields: List[c.universe.Tree]): c.universe.Tree = {
+    import c.universe._
+
+    ensureThriftTypeUnsafe(c)(typ)
+    val dst = TermName(c.freshName)
+    val assignments = fields.zipWithIndex.map { case (f, i) =>
+      q"""$dst.${TermName(s"_${i+1}")} = $f"""
+    }
+
+    val numFields = fieldsUnsafe(c)(typ).length
+    if (fields.length != numFields)
+      c.abort(
+        c.enclosingPosition,
+        s"Can't construct $typ from provided fields. Got code for ${fields.length} fields but needed $numFields"
+      )
+
+    q"""
+      val $dst = new $typ()
+      ..$assignments
+      $dst
+    """
+  }
+
+  /**
+    * Creates the code to construct a Scrooge thrift struct of the specified type given code to
+    * generate values for each field.
+    */
+  def constructScroogeUnsafe(c: Context)(typ: c.universe.Type, fields: List[c.universe.Tree]): c.universe.Tree = {
+    import c.universe._
+
+    ensureThriftTypeUnsafe(c)(typ)
+    val companion = typ.typeSymbol.companion
+    q"$companion.apply(..$fields)"
+  }
 }

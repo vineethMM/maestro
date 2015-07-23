4301671a9db603199d86adf123cb3169bc75e4c7
Stephan Hoermann <srhoermann@gmail.com>
2015-07-23 11:20:50 +1000

    Upgrades to laetest ebenezer version which should spam less exception massages in tests.

--- project/build.scala
+++ project/build.scala
@@ -35,7 +35,7 @@ object build extends Build {
   type Sett = Def.Setting[_]
 
   val thermometerVersion = "1.0.0-20150513002558-a6bcf7f"
-  val ebenezerVersion    = "0.18.3-20150630045019-50d44ae"
+  val ebenezerVersion    = "0.18.4-20150722003606-87cb2f0"
   val omnitoolVersion    = "1.10.0-20150430044321-3ca9118"
   val permafrostVersion  = "0.10.0-20150615051457-13dbbaa"
   val edgeVersion        = "3.4.0-20150513004502-4b6d30d"
